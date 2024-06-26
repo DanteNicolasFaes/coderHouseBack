5c3031d9f89c2b0252e24dc6e890efd8229dc1d6
DanteNicolasFaes <dn.faes@gmail.com>
2024-06-26 07:35:06 -0300

    modifico errores

--- servidor.js
+++ servidor.js
@@ -1,16 +1,15 @@
-// servidor.js
 import express from 'express';
-import bodyParser from 'body-parser'; // Importar body-parser
+import bodyParser from 'body-parser'; 
 import productosRouter from './rutas/productos.js';
 import carritosRouter from './rutas/carritos.js';
 
 const app = express();
 const puerto = 8080;
 
-// Middleware para parsear el body de las peticiones JSON
+
 app.use(bodyParser.json());
 
-// Rutas para productos y carritos
+
 app.use('/api/products', productosRouter);
 app.use('/api/carts', carritosRouter);
 
